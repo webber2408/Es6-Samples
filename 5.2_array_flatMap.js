232ab4bf53e4f0ae1528605019e17ab9f0536c7a
webber2408 <webcrat.tech@gmail.com>
2020-07-14 16:10:24 +0530

    Codes

--- 5.2_array_flatMap.js
+++
@@ -1,7 +0,0 @@
-let arr_string = ["it's Sunny in", "", "California"];
-
-console.log(arr_string.map(x => x.split(" ")));
-// [["it's","Sunny","in"],[""],["California"]]
-
-console.log(arr_string.flatMap(x => x.split(" ")));
-// ["it's","Sunny","in", "", "California"]
