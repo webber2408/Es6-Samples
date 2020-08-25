842804e6a91c06c6f4f906f8c472e7614373a0ae
webber2408 <webcrat.tech@gmail.com>
2020-08-25 20:24:05 +0530

    codes

---
+++ 10.2_Closures_Great_Example.js
@@ -0,0 +1,66 @@
+// Great Example
+
+for (var i=1; i<=5; i++) {
+    setTimeout( function timer(){
+        console.log( i );
+    }, i*1000 );
+}
+// prints 6 for 5 times! -- Why?
+// i values are not enclosed in a closure
+
+// So we put it in an IIFE as we know that execute immediately 
+// and maintain their own scope.
+for (var i=1; i<=5; i++) {
+    (function(){
+        setTimeout( function timer(){
+            console.log( i );
+        }, i*1000 );
+    })();
+}
+
+// Still doesn't work?
+// Coz, what's the use of using an enclosing scope if it's
+// not enclosing anything??
+
+// So, time for some magic?
+for (var i=1; i<=5; i++) {
+    (function(){
+        var j = i;
+        setTimeout( function timer(){
+            console.log( j );
+        }, j*1000 );
+    })();
+}
+// This works flawlessly
+
+// or you could do,
+
+for (var i=1; i<=5; i++) {
+    (function(j){
+        setTimeout( function timer(){
+            console.log( j );
+        }, j*1000 );
+    })(i);
+}
+
+// Method 2 to implement closure via block-scoping
+for (var i=1; i<=5; i++) {
+    let j = i; // yay, block-scope for closure!
+    setTimeout( function timer(){
+        console.log( j );
+    }, j*1000 );
+} // Works perfectly fine!
+
+
+/*
+There’s a special behavior
+defined for let declarations used in the head of a for loop. This behavior says
+that the variable will be declared not just once for the loop, but each iteration.
+And, it will, helpfully, be initialized at each subsequent iteration
+with the valuefrom the end of the previous iteration.
+*/
+for (let i=1; i<=5; i++) {
+    setTimeout( function timer(){
+        console.log( i );
+    }, i*1000 );
+} //works perfectly fine
--- 10_closures_covered.js
+++ 10_closures_covered.js
@@ -15,6 +15,13 @@ function closureExample(nameArg){
 }
 let getName = closureExample("Bellamy");
 getName(); // Bellamy
+// In this case, getName() has a lexical scope closure over the inner scope 
+// of closureExample() function, which keeps the scope alive for getName() to reference at 
+// any later time & hence the scope variables are not collected by the garbage collector
+// even if the function has exited!
+// getName() still has a reference to the inner scope of the closureExample() function
+// and that reference is called a closure.
+// Also, to note, here getName() is being executed outside of its declared lexical scope.
 
 //Example 2
 // In a closure JS Variables are kept by reference
@@ -30,3 +37,21 @@ function fun(){
 
 let getNum = fun();
 getNum(); //4
+
+
+// Example
+function wait(message) {
+    setTimeout( function timer(){
+        console.log( message ); // logs Hello, closure!
+    }, 1000 );
+}
+wait( "Hello, closure!" );
+
+
+/*
+
+Be that timers, event handlers, Ajax requests, cross-window messaging, web workers, 
+or any of the other asynchronous (or synchronous!) tasks, everything has a closure.
+
+*/
+
